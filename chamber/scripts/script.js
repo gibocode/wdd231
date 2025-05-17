be1fe4f5033356663e60aa3739556cf61bd82a81
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-05-18 05:05:54 +0800

    W02 Chamber Directory Page

---
+++ chamber/scripts/script.js
@@ -0,0 +1,92 @@
+// Navigation Menu
+const menuButton = document.querySelector("#menu-button");
+const nav = document.querySelector("#nav");
+
+menuButton.addEventListener("click", () => {
+	nav.classList.toggle("show");
+});
+
+// Footer
+const currentYear = document.querySelector("#current-year");
+const lastModified = document.querySelector("#last-modified");
+const currentDate = new Date();
+
+currentYear.innerHTML = currentDate.getFullYear();
+lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+
+// Member List
+async function renderMemberList() {
+    const memberList = document.querySelector("#member-list");
+    const template = document.querySelector("#member-template");
+    const memberData = await getMemberData();
+
+    memberList.innerHTML = "";
+
+    memberData.forEach(member => {
+        const clone = document.importNode(template.content, true);
+        const memberName = clone.querySelector(".member-name");
+        const memberImage = clone.querySelector(".member-image");
+        const memberEmail = clone.querySelector(".member-email");
+        const memberPhone = clone.querySelector(".member-phone");
+        const memberUrl = clone.querySelector(".member-url");
+        const memberAddress = clone.querySelector(".member-address");
+        const memberLevel = clone.querySelector(".member-level");
+        // const memberDescription = clone.querySelector(".member-description");
+
+        memberName.textContent = member.name;
+        memberImage.src = `images/${member.image}`;
+        memberImage.alt = member.name;
+        memberEmail.href = `mailto:${member.email}`;
+        memberEmail.textContent = member.email;
+        memberPhone.href = `tel:${member.phone}`;
+        memberPhone.textContent = member.phone;
+        memberUrl.textContent = member.website;
+        memberUrl.href = member.website;
+        memberAddress.textContent = member.address;
+        memberLevel.textContent = `Membership Level ${member.membership_level}`;
+        // memberDescription.textContent = member.description;
+
+        memberList.appendChild(clone);
+    });
+
+}
+
+async function getMemberData() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+    return data;
+}
+
+renderMemberList();
+
+// Grid/List
+const gridButton = document.querySelector("#grid-button");
+const listButton = document.querySelector("#list-button");
+
+gridButton.addEventListener("click", () => {
+    switchView("grid");
+});
+
+listButton.addEventListener("click", () => {
+    switchView("list");
+});
+
+function switchView(view) {
+    const memberList = document.querySelector("#member-list");
+    const button = document.querySelector(`#${view}-button`);
+    const otherButton = document.querySelector(`#${view === "grid" ? "list" : "grid"}-button`);
+    button.classList.add("active");
+    otherButton.classList.remove("active");
+    if (view === "grid") {
+        memberList.classList.remove("list-view");
+        memberList.classList.add("grid-view");
+    } else if (view === "list") {
+        memberList.classList.remove("grid-view");
+        memberList.classList.add("list-view");
+    }
+    localStorage.setItem("view", view);
+}
+
+const view = localStorage.getItem("view") || "grid";
+
+switchView(view);
