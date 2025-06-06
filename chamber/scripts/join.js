bf11c1cbdd2d89f54c01efb7e29d74ba7a19f7c2
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-06-06 20:06:39 +0800

    Finalized join page.

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -24,3 +24,37 @@ function hideModal(level) {
     const modal = document.querySelector(`#${level}-level-modal`);
     modal.close();
 }
+
+const timestampField = document.querySelector("#timestamp");
+
+if (timestampField) {
+    timestampField.value = Date.now();
+}
+
+function getParam(name) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(name);
+}
+
+function setTextContent(element, text) {
+    if (element) {
+        element.innerHTML = text;
+    }
+}
+
+const fields = [
+    "firstname",
+    "lastname",
+    "email",
+    "phone",
+    "business-name",
+    "timestamp"
+];
+
+fields.forEach(field => {
+    let text = getParam(field);
+    if (field == "timestamp") {
+        text = (new Date(Number(text))).toLocaleString();
+    }
+    setTextContent(document.querySelector(`#${field}`), text);
+});
